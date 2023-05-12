30a4bb1b48db9a5d92836d1c1bb4e03aa1536eb9
[Github <beomsuong0406@gmail.com>
2023-05-13 02:03:20 +0900

    메모목록

--- lib/memo.dart
+++ lib/memo.dart
@@ -3,21 +3,72 @@ import 'package:metro_beom/provider/mydata.dart';
 import 'package:provider/provider.dart';
 
 class Memo extends StatefulWidget {
-  const Memo({super.key});
+  const Memo({Key? key}) : super(key: key);
 
   @override
   State<Memo> createState() => _MemoState();
 }
 
 class _MemoState extends State<Memo> {
+  Widget room(String a) {
+    // 톡방을 리스트로 보여주는 함수
+    return InkWell(
+      onTap: () {
+        ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text(a)));
+      },
+      child: SizedBox(
+        height: 80,
+        child: Padding(
+          padding: const EdgeInsets.only(top: 8), // 톡방 간 간격
+          child: Row(
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(left: 8),
+                child: SizedBox(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start, // 글자 왼쪽 정렬
+                    children: [
+                      Text(
+                        a,
+                        style: const TextStyle(
+                            fontSize: 15, fontWeight: FontWeight.w600),
+                        // 톡방 제목은 굵게
+                      ),
+                      const Text('가장최근 메모가 나오게 할거에요'),
+                    ],
+                  ),
+                ),
+              ),
+              const Divider(
+                thickness: 2,
+                height: 3,
+                color: Colors.black,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    Mydata data = Provider.of<Mydata>(context);
+    final data = Provider.of<Mydata>(context); // Provider로부터 Mydata 가져오기
+
     return Scaffold(
-        body: Center(
-      child: Text(
-        context.watch<Mydata>().datas.toString(),
+      body: ListView(
+        children: [
+          for (var key in data.datas.keys) room(key),
+        ],
       ),
-    ));
+      floatingActionButton: FloatingActionButton(
+        tooltip: '시간표를 추가합니다',
+        onPressed: () {
+          ScaffoldMessenger.of(context)
+              .showSnackBar(const SnackBar(content: Text('메모를 추가합니다')));
+        },
+        child: const Icon(Icons.add),
+      ),
+    );
   }
 }
